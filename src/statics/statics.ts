2f0c7cc82f2ada3f6658c2155fa34d8edb01248f
zk922 <zhangk922@163.com>
2018-08-02 17:11:11 +0800

    init socket.io

--- src/statics/statics.ts
+++ src/statics/statics.ts
@@ -4,7 +4,10 @@ import * as server from "koa-static";
 
 const app = new Koa();
 
-app.use(server(__dirname + '/www'));
+app.use(server(__dirname + '/www',  {
+  maxAge: 1000 * 60 * 60 * 24 * 7,
+  gzip: true,
+}));
 
 
 export default app;
