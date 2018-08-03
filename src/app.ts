5fad9ffff288d9e2333ecd69e7d7a2589a17a59d
zk922 <zhangk922@163.com>
2018-08-03 15:48:52 +0800

    调整结构，添加readme

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import * as Koa from "koa";
 import * as mount from "koa-mount";
-import statics from "./statics/statics";
 import * as http from "http";
 import * as io from 'socket.io';
+import * as bodyparser from "koa-bodyparser";
+
 import api from "./api/api";
 import addIoMiddleware from "./io/io"
+import statics from "./statics/statics";
+import {serverConfig} from "./config";
 
 async function serverStart() {
   const app = new Koa();
@@ -13,6 +16,9 @@ async function serverStart() {
    * 静态资源仅包含前端资源，使用ng打包，发布在statics/www/目录下
    * api中包含api路由，包含所有类型增改删查的接口
    * */
+  app.use(bodyparser({
+    enableTypes: ['json', 'form', 'text']
+  }));
   app.use(mount('/', statics));
   app.use(mount('/api', api));
 
@@ -22,20 +28,34 @@ async function serverStart() {
    * 只有在聊天室或者私聊时候才需要创建io链接
    * **/
   const server = http.createServer(app.callback());
-  const chat = io(server,{
-    path: '/chat'  //io资源路径设置为/chat
+  const publicChat = io(server,{
+    path: '/chat/public'
+  });
+
+  const groupChat = io(server,{
+    path: '/chat/group'
+  });
+
+  const privateChat = io(server,{
+    path: '/chat/private'
+  });
+
+  const friendsChat = io(server,{
+    path: '/chat/friends'
   });
 
   /**
    * 添加io中间件
    * **/
-  addIoMiddleware(chat);
+
+
+
 
   /**
    * 启动服务器
    * **/
-  server.listen(8088, () => {
-    console.log('service is running at port 8088');
+  server.listen(serverConfig.port, () => {
+    console.log('service is running at port ' + serverConfig.port);
   });
 }
 export default serverStart;
