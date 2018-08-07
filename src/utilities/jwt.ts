fe52daf913fa25ad01eb213a293050cfa11f5d98
zk922 <zhangk922@163.com>
2018-08-07 11:45:57 +0800

    dev

--- src/utilities/jwt.ts
+++ src/utilities/jwt.ts
@@ -1,7 +1,10 @@
-import * as base64 from "js-base64";
 import * as crypto from "crypto";
 import {model} from "mongoose";
 
 
 import {authentication} from "../mongoose/mongooseSchema";
+
+const AuthModel = model('AuthModel', authentication);
+
+
 // const hash = crypto.createHmac('sha256', )
