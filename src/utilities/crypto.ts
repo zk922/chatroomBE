4aff53842269758d0098e9321c64cdece90c31cd
zk922 <zk922@163.com>
2018-08-13 22:01:00 +0800

    jwt

--- src/utilities/crypto.ts
+++ src/utilities/crypto.ts
@@ -2,30 +2,30 @@ import * as crypto from "crypto";
 import TypedArray = NodeJS.TypedArray;
 
 /**
- * 生成随机字符串，使用randomBytes随机字节，转成hex字符串
+ * 生成随机字符串，使用randomBytes随机字节，转成base64字符串
  * **/
-export function createSalt(sizeOfBytes: number): Promise<string> {
+export function createSalt(sizeOfBytes: number, encode: string = 'base64'): Promise<string> {
   return new Promise((resolve, reject)=>{
     crypto.randomBytes(sizeOfBytes, (err, buf)=>{
       if(err){
         reject(err);
         return;
       }
-      resolve(buf.toString('hex'));
+      resolve(buf.toString(encode));
     });
   })
 }
 
 /***
- * sha256 hmac
+ * sha256 hmac，默认生成base64格式
  * */
-export function createSha256Hmac(data: string | Buffer, salt: string | Buffer | TypedArray): Promise<string> {
+export function createSha256Hmac(data: string | Buffer, salt: string | Buffer | TypedArray, encode: string = 'base64'): Promise<string> {
   return new Promise((resolve, reject)=>{
     const hmac = crypto.createHmac('sha256', salt);
     hmac.on('readable', ()=>{
       const data = hmac.read() as Buffer;
       if(data){
-        resolve(data.toString('hex'));
+        resolve(data.toString(encode));
       }
       else{
         reject(new Error('加密失败'));
@@ -35,3 +35,4 @@ export function createSha256Hmac(data: string | Buffer, salt: string | Buffer | 
     hmac.end();
   })
 }
+
--- src/utilities/jwt.ts
+++ src/utilities/jwt.ts
@@ -1,4 +1,4 @@
-import {createSalt, createSha256Hmac} from "./crypto";
+import {createSha256Hmac} from "./crypto";
 
 /**
  * 自己简单生成jwt
@@ -16,9 +16,10 @@ import {createSalt, createSha256Hmac} from "./crypto";
 /**
  * @param u_id: 用户的唯一标识
  * @param u_name 用户的昵称
+ * @param salt 加密盐值
  * @return Promise<string>   string为最终的token字符串
  * **/
-export async function createJWT(u_id, u_name) {
+export async function createJWT(u_id: string, u_name: string, salt: string) {
   const date = new Date();
   const header = {
     "alg": "HS256",
@@ -29,6 +30,19 @@ export async function createJWT(u_id, u_name) {
     "exp": date.getTime() + 7 * 24 * 60 * 60 * 1000,  //有效期为7天
     "u_id": u_id,
     "u_name": u_name
-  }
+  };
+
+  const headerBase64 = new Buffer(JSON.stringify(header)).toString('base64');
+
+  const payloadBase64 = new Buffer(JSON.stringify(payload)).toString('base64');
+
+  const signature = await createSha256Hmac(`${headerBase64}.${payloadBase64}`, salt);
+
+  return `${headerBase64}.${payloadBase64}.${signature}`;
 
 }
+
+
+createJWT('11', '22', 'dsfsaf3').then(v=>{
+  console.log(v);
+});
