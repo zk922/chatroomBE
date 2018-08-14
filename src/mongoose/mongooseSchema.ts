daa2629adbd42d434ad0745eb6f93fc1ad43e66d
zk922 <zhangk922@163.com>
2018-08-14 18:53:37 +0800

    设计数据库存储结构

--- src/mongoose/mongooseSchema.ts
+++ src/mongoose/mongooseSchema.ts
@@ -1,55 +1,45 @@
 import {Schema} from "mongoose";
 
-export namespace userSchemas{
 
-  export const User = new Schema({
-    u_id: String,
-    u_name: String,
-    groups: [groupSchemas.BaseGroup],
-    friends: [friendSchemas.Friend]
-  });
+/**
+ * 好友关系模型，主键为自动创建的id
+ * 也就是用户和用户之间的对应模型
+ * **/
+export const FriendShip = new Schema({  //好友关系模型
+  user1: {
+    u_id: String,  //用户1的id
+    note: String   //用户1在对方的备注
+  },
+  user2: {
+    u_id: String,  //用户2的id
+    note: String   //用户2在对方的备注
+  }
+});
 
-  export const UserInGroup = new Schema({
-    u_id: String,
-    u_name: String,
-    identity: Number
-  });
-}
+/**
+ * 群组关系模型，主键为自动创建的id
+ * **/
+export const Group = new Schema({
+  name: String,   //群组名称
+  users: []
+});
 
-export namespace groupSchemas {
-  export const BaseGroup = new Schema({
-    group_id: String,
-    group_name: String
-  });
-  export const Group = new Schema({
-    group_id: String,
-    group_name: String,
-    users: [userSchemas.UserInGroup]
-  });
-}
+/**
+ * 用户和群组的map模型
+ * **/
+export const UserInGroup = new Schema({
 
-export namespace friendSchemas {
-
-  export const Friend = new Schema({
-    u_id: String,
-    name: String,
-    friend_ship_id: String
-  });
-
-}
+});
 
 
-export namespace friendShipSchemas {
-  export const FriendShip = new Schema({
-    u_id_1: String,
-    u_id_2: String
-  });
-}
+export const User = new Schema({
+  //认证信息相关
+  name: String,  //账号
+  pwd: String,   //密码
+  secret: String,//token的salt
 
-
-export const authentication = new Schema({
-  name: String,
-  pwd: String,
-  u_id: String,
-  secretKey: String
+  //用户信息相关
+  nickName: String,   //用户昵称
+  friends: [FriendShip],        //用户朋友关系列表
+  groups: [UserInGroup]          //用户群组关系列表
 });
