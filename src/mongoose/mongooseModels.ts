28ee6a330290b23d344f432039a07ff2699f397c
zk922 <zk922@163.com>
2018-08-14 22:53:09 +0800

    注册逻辑完成，但没有防止刷接口

--- src/mongoose/mongooseModels.ts
+++ src/mongoose/mongooseModels.ts
@@ -3,16 +3,18 @@ import {
   model,                          //use the defaule connetion  (monggose.connection)
   Schema
 } from "mongoose";
-import {groupSchemas, authentication, userSchemas} from "./mongooseSchema";
+import {User, UserInGroup, FriendShip, Group} from "./mongooseSchema";
 
 /**
  * mongoose models 是用来存储和查询数据的。
  *
  * 一个monggose可以创建多条与MongoDB的链接，这个项目里面，仅使用一条链接即可。
  * **/
-export const UserModel = model('User', userSchemas.User);
+export const UserModel = model('User', User);
 
-export const GroupModel = model('Group', groupSchemas.Group);
+export const GroupModel = model('Group', Group);
 
-export const AuthModel = model('Auth', authentication);
+export const FriendShipModel = model('FriendShip', FriendShip);
+
+export const UserInGroupModel = model('UserInGroup', UserInGroup);
 
