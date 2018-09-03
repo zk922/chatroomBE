0e67d30eac62131109a603b23b6dfa8e71b36e76
zk922 <zhangk922@163.com>
2018-09-03 18:45:16 +0800

    socket链接建立时候，进行认证

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,17 +1,21 @@
-import {Socket} from "socket.io";
-
+// import {Socket} from "socket.io";
+import {Document} from "mongoose";
 
 export class User {
 
+  public u_id: string;
 
-  constructor(socket: Socket){
+  public nickName: string;
 
+  public friends: string[];
 
+  public groups: string[];
 
-
+  constructor(document: Document){
+    this.u_id = document.id;
+    this.groups = document.get('groups');
+    this.nickName = document.get('nickName');
+    this.friends = document.get('friends')
   }
-
-
-
 }
 export default User;
