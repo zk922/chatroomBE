4f68058a8a001d7ea8fc2c1d8e47451c55049ccb
zk922 <zhangk922@163.com>
2018-09-18 11:40:48 +0800

    调整结构，实现socket.io基于event的路由

--- src/models/IoRouter.ts
+++ src/models/IoRouter.ts
@@ -7,47 +7,72 @@ import RouterPacket = ioServerInterfaces.RouterPacket;
 import RouterMiddleware = ioServerInterfaces.RouterMiddleware;
 import EnhancedPacket = ioServerInterfaces.EnhancedPacket;
 
-
-
-
 export class IoRouter {
 
-  constructor(){
+  constructor(){}
 
-  }
 
+  /**
+   * 根据添加的路由中间件，返回io service使用的中间件
+   * **/
   public routes(): EnhancedMiddleware{
-
-    //TODO developing
     return (packet: EnhancedPacket, next: (err?: any) => void)=>{
       this.mwList.forEach((v, i)=>{
-
+        let path = packet.packet[0];
+        //进行path/event路径匹配
+        if(v.reg.test(path)){
+          //解析参数
+          let data = {};
+          let values = path.match(v.reg).slice(1);
+          v.keys.forEach((v,i)=>{
+            data[v] = values[i];
+          });
+          //传入扩展后的路由中间件
+          //注：这里没有更改this指向
+          let routerPacket: RouterPacket = Object.assign({data: data, path: path}, packet);
+          v.mw(routerPacket, next);
+        }
       });
     };
   }
 
-
-  private mwList: {path: string, mw: RouterMiddleware}[] = [];
-
+  private mwList: {path: string, mw: RouterMiddleware, reg: RegExp, keys: string[]}[] = [];
+  /**
+   * 添加路由中间件，path实际就是socket.io的自定义event
+   * @param path: string
+   * @param middleware: RouterMiddleware
+   * **/
   public use(path: string, middleware: RouterMiddleware): IoRouter{
-    this.mwList.push({
+    this.mwList.push(Object.assign({
       path: path,
       mw: middleware
-    });
+    }, this.getPathRegAndKeys(path)));
     return this;
   }
 
-  private parsePath(){
-
-  }
-
-  private getPathReg(path: string): RegExp{
+  /**
+   * 通过use添加io中间件时，生成path解析正则与参数列表
+   * 本例子中的参数简单参数都是直接添加在路径中，比如'/path1/:id/:name',key为id与name
+   * @param path: string       中间件对应的路径
+   * **/
+  private getPathRegAndKeys(path: string): {keys: string[], reg: RegExp}{
     let pathSegments = path.replace(' ', '').split('/').filter(v=>v!=='');
-    let regStr = '';
-    //TODO
-    return /\d/
-
+    let regStr = '^[/\\\\]?';
+    let keys = [];
+    pathSegments.forEach(v => {
+      if(/^:\w+$/.test(v)){
+        regStr += '(\\w+)[/\\\\]';
+        keys.push(v.slice(1));
+      }
+      else{
+        regStr += (v + '[/\\\\]');
+      }
+    });
+    regStr += '?$';
+    return {
+      keys: keys,
+      reg: new RegExp(regStr)
+    };
   }
-
 }
 export default IoRouter;
